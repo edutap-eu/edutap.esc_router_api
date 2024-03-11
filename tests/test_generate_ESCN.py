668fbd499f5b2b521594e995c9b10a484e61cd1e
Alexander Loechel <Alexander.Loechel@lmu.de>
2024-03-11 12:17:25 +0100

    make test more explicite and test more things, variant, version and invalid PIC

--- tests/test_generate_ESCN.py
+++ tests/test_generate_ESCN.py
@@ -1,4 +1,7 @@
+from edutap.esc_router_api.utils import ESCN_Factory_Exception
 from edutap.esc_router_api.utils import generate_ESCN
+
+import pytest
 import uuid
 
 
@@ -7,6 +10,8 @@ def test_generate_ESCN():
 
     escn = generate_ESCN(pic=PIC)
     assert isinstance(escn, uuid.UUID)
+    assert escn.variant == uuid.RFC_4122
+    assert escn.version == 1
     assert str(escn)[24:] == "001" + PIC
 
 
@@ -17,3 +22,10 @@ def test_generate_ESCN_with_prefix():
     escn = generate_ESCN(pic=PIC, prefix=PREFIX)
     assert isinstance(escn, uuid.UUID)
     assert str(escn)[24:] == "003" + PIC
+
+
+def test_generate_ESCN__invalid_PIC():
+    PIC = "1234567890"
+
+    with pytest.raises(ESCN_Factory_Exception):
+        generate_ESCN(pic=PIC)
